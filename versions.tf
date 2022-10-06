b72981ebffbea9d43af6956c7649878faa2cbb83
Steven Feltner <steven.feltner@netapp.com>
2022-10-05 23:49:07 -0700

    updates

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 0.13.1"
 
   required_providers {
     spotinst = {
       source  = "spotinst/spotinst"
-      version = ">= 1.27.0"
+      version = ">= 1.78"
     }
   }
 }
